78c9eed0adea75a4678584dfb30833cc0f9fbcfb
Mathis Durand <mathis.durand@hotmail.com>
2026-02-01 18:13:32 +0100

    v1 done

--- src/processor.py
+++ src/processor.py
@@ -4,34 +4,56 @@ import torch
 import torch.nn.functional as F
 
 
-# Step 1: fetch news form the database
-
-conn = get_connection()
-cursor = conn.cursor()
-
-sql_query = """
-        SELECT id, title, description FROM news WHERE sentiment_label IS NULL
-        """
-
-cursor.execute(sql_query)
-rows = cursor.fetchall()
-
-# Step 2: load pre-trained model and tokenizer
 model_name = "ProsusAI/finbert" 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-# Step 3: perform sentiment analysis and update the database
-for row in rows:
-    content = f"{row[1]}. {row[2]}" # Combine title and description
-    inputs = tokenizer(content, return_tensors="pt")
-    labels = model.config.id2label
+def process_sentiments():
+    conn = get_connection()
+    cursor = conn.cursor()
 
-    with torch.no_grad():
-        outputs = model(**inputs)
-        logits = outputs.logits
-        probabilities = F.softmax(logits, dim=-1)
-        predicted_class_id = torch.argmax(probabilities).item()
-        sentiment_label = labels[predicted_class_id]
-        sentiment_score = probabilities[0][predicted_class_id].item()
-        
+    sql_query = """
+            SELECT id, title, description FROM news WHERE sentiment_label IS NULL
+            """
+
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+    if not rows:
+        print("No new articles to process.")
+        return 
+
+    print(f"Processing {len(rows)} articles...")
+
+    data_to_update = []
+    for row in rows:
+        title = row[1]
+        description = row[2] if row[2] else ""
+        content = f"{title}. {description}" # Combine title and description
+        inputs = tokenizer(content, return_tensors="pt", truncation=True, max_length=512, padding=True) # Truncate long texts, set max length because of model limits and padding is needed for batch processing (even if single input, puts "" tokens to make up length)
+        labels = model.config.id2label
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+            logits = outputs.logits
+            probabilities = F.softmax(logits, dim=-1)
+            predicted_class_id = torch.argmax(probabilities).item()
+            sentiment_label = labels[predicted_class_id]
+            sentiment_score = probabilities[0][predicted_class_id].item()
+            data_to_update.append((sentiment_label, sentiment_score, row[0]))
+
+    if data_to_update:
+        update_query = """
+            UPDATE news
+            SET sentiment_label = ?, sentiment_score = ?
+            WHERE id = ?
+            """
+        cursor.executemany(update_query, data_to_update)
+        conn.commit()
+        print(f"Successfully updated {len(data_to_update)} articles.")
+
+    conn.close()
+
+### -------------------------------------------------------------------------------------------------------------- ###
+
+if __name__ == "__main__":
+    process_sentiments()
